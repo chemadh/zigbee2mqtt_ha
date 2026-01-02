c64fa67c2a719f1baff972aaea588cbf8f2a963c
chemadh <111968391+chemadh@users.noreply.github.com>
2026-01-02 01:06:45 +0100

    Add files via upload

--- scripts/homeassistant_os/activeZigbee2mqtt2.sh
+++ scripts/homeassistant_os/activeZigbee2mqtt2.sh
@@ -1,10 +1,186 @@
 #!/bin/bash
 
-ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa -f root@192.168.34.130 'rm /config/sh/result.log; nohup /config/sh/remote/activeZigbee2mqtt2.sh >/config/sh/result.log 2>&1 </dev/null &'
+zigbee2mqtt1_remote_ssh="zigbee@192.168.34.123"
+zigbee2mqtt2_remote_ssh="zigbee@192.168.34.124"
+zigbee2mqtt_stop_cmd="sudo rc-service zigbee2mqtt stop"
+zigbee2mqtt_start_cmd="sudo rc-service zigbee2mqtt start"
+snmp_server="192.168.34.103:1234"
+snmp_host="homeassistant"
+coordinator_port_zigbee2mqtt1="/dev/ttyUSB0"
+coordinator_port_zigbee2mqtt2="/dev/ttyUSB0"
+zigbee2mqtt_nvram_path="/home/zigbee/scripts/"
+zigbee2mqtt_nvram_file="backup_coordinator_nvram.json"
+local_aux_dir="/home/zigbee/scripts/zigbee2mqtt_config/"
+local_conf_dir="/opt/zigbee2mqtt/data/"
 
-#ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa -f domo@192.168.34.125 'rm ./scripts/result.log; nohup ./scripts/longExecTimeScript.sh >./scripts/result.log 2>&1 </dev/null &'
+exit_code=0
 
-#ejecutando en background para evitar cancelación de script por timeout en homeassistant 
-#cd /config/sh/
-#rm ./nohup.out
-#nohup ./activeZigbee2mqtt2_background.sh & disown
+# stopping zigbee2mqtt1
+
+command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt1_remote_ssh} '${zigbee2mqtt_stop_cmd}'"
+echo "$command"
+if echo "$command" | bash ; then
+        echo "first zigbee2mqtt1 stop ok"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "first zigbee2mqtt1 stop ok" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+else
+        echo "first zigbee2mqtt1 stop error"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "first zigbee2mqtt1 stop error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+fi
+# executing the command twice (Alpine service stop sometimes return a ghost error)
+if echo "$command" | bash ; then
+        echo "second zigbee2mqtt1 stop ok"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "second zigbee2mqtt1 stop ok" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+else
+        echo "second zigbee2mqtt1 stop error"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "second zigbee2mqtt1 stop error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+fi
+
+# stopping zigbee2mqtt2 (just in case it was already running. Required to operate with coordinator NVRAM)
+command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt2_remote_ssh} '${zigbee2mqtt_stop_cmd}'"
+echo "$command"
+if echo "$command" | bash ; then
+        echo "first zigbee2mqtt2 stop OK"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "first zigbee2mqtt2 stop OK" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+else
+        echo "first zigbee2mqtt2 stop error"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "first zigbee2mqtt2 stop error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+        fi
+fi
+# executing the command twice (Alpine service stop sometimes return a ghost error)
+if echo "$command" | bash ; then
+        echo "second zigbee2mqtt2 stop OK"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "second zigbee2mqtt2 stop OK" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+else
+        echo "second zigbee2mqtt2 stop error"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "second zigbee2mqtt2 stop error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+        fi
+fi
+
+# Retrieving NVRAM memory backup from zigbee coordinator USB dongle of zigbee2mqtt1 node (expected to be the last active node)
+command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt1_remote_ssh} 'source venv/bin/activate && sudo python -m zigpy_znp.tools.nvram_read ${coordinator_port_zigbee2mqtt1} -o ${zigbee2mqtt_nvram_path}${zigbee2mqtt_nvram_file} && deactivate'"
+echo "$command"
+if echo "$command" | bash ; then
+        echo "coordinator zigbee2mqtt1 NVRAM backup OK"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "Coordinator zigbee2mqtt1 NVRAM backup OK" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+
+        # synchronization of coordinator NVRAM dump file from zigbee2mqtt1 to zigbee2mqtt2 node
+        command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt1_remote_ssh} 'rsync -avuP ${zigbee2mqtt_nvram_path}${zigbee2mqtt_nvram_file} ${zigbee2mqtt2_remote_ssh}:${zigbee2mqtt_nvram_path}'"
+        echo "$command"
+        if echo "$command" | bash ; then
+                echo "coordinator NVRAM backup sync to Zigbee2mqtt2 OK"
+                if ! [[ -z "$snmp_server" ]]; then
+                        snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "coordinator NVRAM backup sync in Zigbee2mqtt2 OK" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+                fi
+                #cleaning memory of coordinator dongle in zigbee2mqtt2
+                command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt2_remote_ssh} 'source venv/bin/activate && sudo python -m zigpy_znp.tools.nvram_reset ${coordinator_port_zigbee2mqtt2} && deactivate'"
+                echo "$command"
+                if echo "$command" | bash ; then
+                        echo "coordinator zigbee2mqtt2 NVRAM reset OK"
+                        if ! [[ -z "$snmp_server" ]]; then
+                                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "coordinator zigbee2mqtt2 NVRAM reset OK" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+                        fi
+                else
+                        echo "coordinator zigbee2mqtt2 NVRAM reset error"
+                        exit_code=1
+                        if ! [[ -z "$snmp_server" ]]; then
+                                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "coordinator zigbee2mqtt2 NVRAM reset error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+                        fi
+                fi
+
+                # loading NVRAM memory backup file into zigbee2mqtt2 coordinator dongle
+                command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt2_remote_ssh} 'source venv/bin/activate && sudo python -m zigpy_znp.tools.nvram_write ${coordinator_port_zigbee2mqtt2} -i ${zigbee2mqtt_nvram_path}${zigbee2mqtt_nvram_file} && deactivate'"
+                echo "$command"
+                if echo "$command" | bash ; then
+                        echo "coordinator zigbee2mqtt2 NVRAM load OK"
+                        if ! [[ -z "$snmp_server" ]]; then
+                                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "coordinator zigbee2mqtt2 NVRAM load OK" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+                        fi
+                else
+                        echo "coordinator zigbee2mqtt2 NVRAM load error"
+                        exit_code=1
+                        if ! [[ -z "$snmp_server" ]]; then
+                                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "coordinator zigbee2mqtt2 NVRAM load error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+                        fi
+                fi
+
+        else
+                echo "coordinator NVRAM backup sync to Zigbee2mqtt2 Error"
+                exit_code=1
+                if ! [[ -z "$snmp_server" ]]; then
+                        snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "coordinator NVRAM backup sync in Zigbee2mqtt2 Error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+                fi
+        fi
+
+
+else
+        echo "coordinator zigbee2mqtt1 NVRAM backup error"
+        exit_code=1
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "coordinator zigbee2mqtt1 NVRAM backup error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+        fi
+fi
+
+# Load synchronized zigbe2mqtt configuration to zigbee2mqtt2 and node startup
+command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt2_remote_ssh} 'sudo rsync -rlpgoDcv ${local_aux_dir}* ${local_conf_dir}'"
+echo "$command"
+if echo "$command" | bash ; then
+        echo "zigbee2mqtt2 config updated (if changed) OK"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "zigbee2mqtt2 config updated (if changed) OK" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+else
+        echo "zigbee2mqtt2 config update errror"
+        exit_code=1
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "zigbee2mqtt2 config update errror" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+        fi
+fi
+
+command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt2_remote_ssh} '${zigbee2mqtt_start_cmd}'"
+echo "$command"
+if echo "$command" | bash ; then
+        echo "zigbee2mqtt2 start OK"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "zigbee2mqtt2 start OK" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+else
+        echo "zigbee2mqtt2 start error"
+        exit_code=1
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "zigbee2mqtt2 start error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+        fi
+fi
+
+# rebooting zigbee2mqtt1 (be sure that zigbee2mqtt is not starting automatically on startup. If it is the case, it should be disabled)
+command="ssh -o UserKnownHostsFile=/config/.ssh/known_hosts -i /config/.ssh/id_rsa ${zigbee2mqtt1_remote_ssh} 'sudo reboot'"
+echo "$command"
+if echo "$command" | bash ; then
+        echo "zigbee2mqtt1 reboot ok"
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "zigbee2mqtt1 reboot ok" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "OK"
+        fi
+else
+        echo "zigbee2mqtt1 reboot error"
+        exit_code=1
+        if ! [[ -z "$snmp_server" ]]; then
+                snmptrap -v 2c -c public "$snmp_server" '' CUSTOM-SCRIPT-MIB::netSnmpScriptResEntry CUSTOM-SCRIPT-MIB::scriptName.0 s "activeZigbee2mqtt2.sh" CUSTOM-SCRIPT-MIB::scriptHost.0 s ""$snmp_host"" CUSTOM-SCRIPT-MIB::scriptMessage.0 s "zigbee2mqtt1 reboot error" CUSTOM-SCRIPT-MIB::scriptStatus.0 s "ERR"
+        fi
+fi
+
+exit $exit_code
